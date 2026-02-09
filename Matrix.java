dd76a117a86da45f41b50afff6164be32f55528f
vish-as09 <rumjhumsingh355@gmail.com>
2026-02-09 10:46:11 +0530

    Add files via upload

---
+++ Matrix.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Matrix {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("\n Enter rows : ");
+		int rows = sc.nextInt();
+		System.out.print("\n Enter columns : ");
+		int cols = sc.nextInt();
+		int [][]a= new int [rows][cols];
+		for(int i=0 ; i<rows ; i++) {
+			for(int j=0 ; j<cols ; j++) {
+				System.out.print("\n a["+i+"]["+j+"] : ");
+				a[i][j]=sc.nextInt();
+			}
+		}
+		System.out.println();
+		for(int i=0 ; i<rows ; i++) {
+			for(int j=0 ; j<cols ; j++) {
+				System.out.print(a[i][j]+" ");
+			}
+			System.out.println();
+			}
+		sc.close();
+	}
+}
